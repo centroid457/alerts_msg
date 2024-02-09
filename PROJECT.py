c4636d6da7f0f7593fc877b9528f201beeb842b1
Andrei Starichenko <a.starichenko@element-t.ru>
2024-02-09 15:35:59 +0300

    apply new pypi template/2

--- PROJECT.py
+++ PROJECT.py
@@ -1,11 +1,13 @@
 from typing import *
+from _aux__release_files import release_files_update
+
+
+# =====================================================================================================================
+VERSION = (0, 0, 3)   # 1/deprecate _VERSION_TEMPLATE from PRJ object +2/place update_prj here in __main__ +3/separate finalize attrs
 
 
 # =====================================================================================================================
 class PROJECT:
-    # AUX --------------------------------------------------
-    _VERSION_TEMPLATE: Tuple[int] = (0, 0, 2)
-
     # AUTHOR -----------------------------------------------
     AUTHOR_NAME: str = "Andrei Starichenko"
     AUTHOR_EMAIL: str = "centroid@mail.ru"
@@ -13,7 +15,6 @@ class PROJECT:
 
     # PROJECT ----------------------------------------------
     NAME_IMPORT: str = "alerts_msg"
-    NAME_INSTALL: str = NAME_IMPORT.replace("_", "-")
     KEYWORDS: List[str] = [
         "alerts", "notifications",
         "email alerts", "smtp", "mail", "email",
@@ -24,13 +25,9 @@ class PROJECT:
         "Topic :: Communications :: Email",
     ]
 
-    # GIT --------------------------------------------------
-    DESCRIPTION_SHORT: str = "All abilities (mail/telegram) to send alert msgs (threading)"
-
     # README -----------------------------------------------
-    pass
-
     # add DOUBLE SPACE at the end of all lines! for correct representation in MD-viewers
+    DESCRIPTION_SHORT: str = "All abilities (mail/telegram) to send alert msgs (threading)"
     DESCRIPTION_LONG: str = """
 designed for ...
     """
@@ -44,7 +41,6 @@ designed for ...
 
     # HISTORY -----------------------------------------------
     VERSION: Tuple[int, int, int] = (0, 2, 1)
-    VERSION_STR: str = ".".join(map(str, VERSION))
     TODO: List[str] = [
         "..."
     ]
@@ -52,13 +48,17 @@ designed for ...
         "..."
     ]
     NEWS: List[str] = [
-        "apply new pypi template"
+        "apply new pypi template/2"
     ]
+
+    # FINALIZE -----------------------------------------------
+    VERSION_STR: str = ".".join(map(str, VERSION))
+    NAME_INSTALL: str = NAME_IMPORT.replace("_", "-")
 
 
 # =====================================================================================================================
 if __name__ == '__main__':
-    pass
+    release_files_update(PROJECT)
 
 
 # =====================================================================================================================
