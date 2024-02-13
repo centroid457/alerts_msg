266c4e224e1d63a6fb8c7ff5ff57b816a8a7b725
Andrei Starichenko <a.starichenko@element-t.ru>
2024-02-13 14:59:10 +0300

    delete http (use in server_template)

--- explore.py
+++ explore.py
@@ -1,8 +1,2 @@
 # DON'T DELETE!
 # useful to start smth without pytest and not to run in main script!
-
-
-from alerts_msg.http import AlertHttp
-
-Victim = AlertHttp
-assert Victim({}).result_wait() is True
