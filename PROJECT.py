9e9a103552414048d9443b600fa30ef7d05d73f3
Andrei Starichenko <centroid@mail.ru>
2024-10-30 17:16:37 +0300

    # ALL MOVED INTO BASE_AUX

--- PROJECT.py
+++ PROJECT.py
@@ -35,17 +35,14 @@ designed for ...
         # "feat1",
         # ["feat2", "block1", "block2"],
 
-        ["send alert msgs", "emails", "telegram", ],
-        "threading",
+
     ]
 
     # HISTORY -----------------------------------------------
     VERSION: Tuple[int, int, int] = (0, 2, 2)
     TODO: List[str] = [
-        "test multyStart by one object (QThread)"
     ]
     FIXME: List[str] = [
-        "NEED SOLVE ABILITY to work without PV.FILE!!! as just a http client"
     ]
     NEWS: List[str] = [
         "[__INIT__.py] fix import",
